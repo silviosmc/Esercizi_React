86853355050cd2ae95fb95aa746648d83c8e03cf
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2023-02-27 18:58:38 +0100

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -5,7 +5,6 @@ export class Hello extends React.Component {
         return(
             <div>
                 <h1>Hello, World!</h1>
-                <p>What a beautiful day!</p>
             </div>
         )
     }
