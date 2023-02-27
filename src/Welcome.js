b1d26266ff98b3f4a97330756315b68b2df9b1f8
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-02-27 20:13:17 +0100

    props05

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render(){
         return(
             <div>
                 <p>Welcome, {this.props.name}</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age={this.props.age}/>
             </div>
         ) 
     }
