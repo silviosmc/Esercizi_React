68ca9fe6ebb5855702e82884e3b412d666e54651
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-12 00:30:50 +0100

    Conditional Rendering - 01

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,12 +5,12 @@ export class Welcome extends React.Component {
     render(){
         return(
             <div>
-                <p>Welcome, {this.props.name}</p>
-                <Age age={this.props.age}/>
+                {this.props.age > 18 && <Age age={this.props.age}/>}
             </div>
         ) 
     }
 }
+
 
 Welcome.defaultProps = {
     name: "Guest"
