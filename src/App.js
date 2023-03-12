2d7c18e0de3fb4f45e7fb81220bec6c45237e5ac
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-12 20:17:48 +0100

    Styling Components - 01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 import { ClickCounter } from './ClickCounter';
 import { ClickTracker } from './ClickTracker';
 import { Counter } from './Counter';
--- src/Welcome.js
+++ src/Welcome.js
@@ -4,7 +4,7 @@ import { Age } from "./Age";
 export class Welcome extends React.Component {
     render(){
         return(
-            <div>
+            <div className='welcome'>
                 <h1>Hello {this.props.name}</h1>
                 <Age age={this.props.age}/>
             </div>
