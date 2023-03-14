143f188c28236de5f296f6b257edf0e746d311e2
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-14 21:40:57 +0100

    React Router - 02

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Container } from './Container';
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from './Welcome';
-
+import { Counter } from './Counter';
 
 
 export function App() {
 
     return(
-        <Container title="React Router 01">
+        <Container title="React Router 02">
 
             <Routes>
                 <Route path="/" element={<Welcome name="Silvio" age={33} />} />
+                <Route path="/counter" element={<Counter />} />
             </Routes>
 
         </Container>
