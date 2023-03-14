4a913101e2602e24948b5dddaab3aa8449ff1229
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-14 22:01:47 +0100

    React Router - 03

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Container } from './Container';
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from './Welcome';
 import { Counter } from './Counter';
+import { ShowGithubUser } from './ShowGithubUser';
 
 
 export function App() {
 
     return(
-        <Container title="React Router 02">
+        <Container title="React Router 03">
 
             <Routes>
                 <Route path="/" element={<Welcome name="Silvio" age={33} />} />
                 <Route path="/counter" element={<Counter />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
             </Routes>
 
         </Container>
---
+++ src/ShowGithubUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { GithubUser } from "./GithubUser";
+
+export function ShowGithubUser() {
+    const {username} = useParams()
+
+    return(
+        <div>
+            {<GithubUser username={username}/>}
+        </div>
+    )
+}
