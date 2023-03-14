5c72070928ca8f47587e1e2fa4faadb1bfc85cc4
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-14 14:40:33 +0100

    ok

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-/* Non ho capito la consegna, devo passare onCounterChange 
-come prop e poi usarla dentro useEffect prendendo come parametro 
-valore, ma cosa deve fare onCounterChange? e dove deve essere inizializzata? */
 
 export function ClickCounter({ onCounterChange }) {
   const [valore, setValore] = useState(0);
+
+
 
   function handleCounter() {
     setValore((valore) => valore + 1);
