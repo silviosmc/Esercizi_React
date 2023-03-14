eaf942779226343e580e29111dd6a147f86e8a92
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-14 22:22:51 +0100

    React Router - 04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Container } from './Container';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Welcome } from './Welcome';
 import { Counter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
 
 
+// Ho utilizzato i button con useNavigate perchè i link per qualche motivo non li renderizzava
+
 export function App() {
 
+    const navigate = useNavigate()
+    
+    function handleHomeButton() {
+        navigate("/")
+    }
+    function handleCounterButton() {
+        navigate("/counter")
+    }
+    function handleUsersButton() {
+        navigate("/users")
+    }
+
     return(
-        <Container title="React Router 03">
+        <Container title="React Router 04">
 
             <Routes>
                 <Route path="/" element={<Welcome name="Silvio" age={33} />} />
                 <Route path="/counter" element={<Counter />} />
-                <Route path="users/:username" element={<ShowGithubUser />} />
+                <Route path="/users/:username" element={<ShowGithubUser />} />
             </Routes>
+            <button onClick={handleHomeButton}>Home</button>
+            <button onClick={handleCounterButton}>Counter</button>
+            <button onClick={handleUsersButton}>Users</button>
 
         </Container>
+        
     )
 
 }
